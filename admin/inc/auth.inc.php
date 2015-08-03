d77b52c4c194edce0060cbb99fd07d31645297e6
nashe <contact@nashe.fr>
2015-08-03 19:00:27 +0200

    Improve coding style

--- admin/inc/auth.inc.php
+++ admin/inc/auth.inc.php
@@ -1,11 +1,9 @@
 <?php
-include (dirname(__FILE__).'/pwd.inc.php');
 
-if ( isset($_COOKIE['auth']) && $_COOKIE['auth'] === $password ) {
-    //ok, cool
-} else {
-    setcookie('auth','', time()-3600);
+include dirname(__FILE__).'/pwd.inc.php';
+
+if (!isset($_COOKIE['auth']) || $_COOKIE['auth'] !== $password) {
+    setcookie('auth', '', time() - 3600);
     header('Location: login.php');
-    die;
+    die();
 }
-?>
