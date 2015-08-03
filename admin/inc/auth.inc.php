6ad38c58a45642eb8c7844e2f272ef199f59550d
nashe <contact@nashe.fr>
2015-08-03 18:51:51 +0200

    Avoid type juggling vulnerability.
    Password comparison should not be done with the `==` operator, but
    `===`, due to type juggling.
    
    References:
    
    * http://phpsadness.com/sad/47
    *
    turbochaos.blogspot.fr/2013/08/exploiting-exotic-bugs-php-type-juggling.
    html
    
    ### Test case
    
    * Create an administrator with the password "240610708".
    * Try to login to the dashboard with the password "QNKCDZO" :-)

--- admin/inc/auth.inc.php
+++ admin/inc/auth.inc.php
@@ -1,7 +1,7 @@
 <?php
 include (dirname(__FILE__).'/pwd.inc.php');
 
-if ( isset($_COOKIE['auth']) && $_COOKIE['auth'] == $password ) {
+if ( isset($_COOKIE['auth']) && $_COOKIE['auth'] === $password ) {
     //ok, cool
 } else {
     setcookie('auth','', time()-3600);
