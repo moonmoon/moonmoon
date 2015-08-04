243ce3d9a243c54d1de3f23a8f7d5424c5aa3dc9
nashe <contact@nashe.fr>
2015-08-04 17:26:42 +0200

    Fix short echo open tags deprecation.
    Short echo open tags are deprecated and disabled by default in php.ini
    since PHP 5.5.0. It does not broke anything for olders versions.

--- custom/views/default/footer.tpl.php
+++ custom/views/default/footer.tpl.php
@@ -1,3 +1,3 @@
         <div id="footer">
-            <p><? echo str_replace('%s', 'href="http://moonmoon.org"', _g('Powered by <a %s>moonmoon</a>'))?> | <a href="./admin/"><?=_g('Administration')?></a></p>
+            <p><?php echo str_replace('%s', 'href="http://moonmoon.org"', _g('Powered by <a %s>moonmoon</a>'))?> | <a href="./admin/"><?=_g('Administration')?></a></p>
         </div>
