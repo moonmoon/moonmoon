cec1a3d21fa822d4065492b217d997a8b1155d28
nashe <thomas@chauchefoin.fr>
2017-05-28 13:53:51 +0200

    Fix minor errors found with debug mode on

--- app/classes/PlanetItem.php
+++ app/classes/PlanetItem.php
@@ -4,14 +4,19 @@
  * Planet item
  */
 
-class PlanetItem
+class PlanetItem extends SimplePie_Item
 {
     public function __construct($feed, $data)
     {
-        parent::SimplePie_Item($feed, $data);
+        parent::__construct($feed, $data);
     }
 
-    public function compare($item1, $item2)
+    /**
+     * @param PlanetItem $item1
+     * @param PlanetItem $item2
+     * @return int
+     */
+    public static function compare($item1, $item2)
     {
         $item1_date = $item1->get_date('U');
         $item2_date = $item2->get_date('U');
