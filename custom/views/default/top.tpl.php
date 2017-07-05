511f35ccd9a6a8285db9a2405ab94ff7a1d93c9e
nashe <thomas@chauchefoin.fr>
2017-07-05 23:53:36 +0200

    Coding style
    As described in #87

--- custom/views/default/top.tpl.php
+++ custom/views/default/top.tpl.php
@@ -1,3 +1,3 @@
         <div id="header">
-            <h1 id="top"><a href="<?php echo $PlanetConfig->getUrl() ?>"><?php echo $PlanetConfig->getName(); ?></a></h1>
+            <h1 id="top"><a href="<?php echo $PlanetConfig->getUrl(); ?>"><?php echo $PlanetConfig->getName(); ?></a></h1>
         </div>
