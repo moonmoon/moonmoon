0d670bba947d825e5cc2a3a89683449b852288f3
nashe <thomas@chauchefoin.fr>
2017-07-07 23:49:51 +0100

    Make PlanetError more testable

--- app/classes/PlanetError.php
+++ app/classes/PlanetError.php
@@ -3,21 +3,30 @@
 class PlanetError
 {
     public $level;
+    public $levels = array(
+        1 => 'notice',
+        2 => 'warning',
+        3 => 'error',
+    );
     public $message;
 
+    /**
+     * PlanetError constructor.
+     * @param $level
+     * @param $message
+     */
     public function __construct($level, $message)
     {
         $this->level = (int) $level;
         $this->message = $message;
     }
 
-    public function toString($format = '%1$s : %2$s')
+    /**
+     * @param  string $format
+     * @return string
+     */
+    public function toString($format = '%1$s: %2$s')
     {
-        $levels = array(
-            1 => 'notice',
-            2 => 'warning',
-            3 => 'error',
-        );
-        return sprintf($format, $levels[$this->level], $this->message);
+        return sprintf($format, $this->levels[$this->level], $this->message);
     }
 }
